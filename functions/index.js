0c60d233b4fa900b6e06fb5fb300f291e25f5265
Leandro Personal <leandrodrazic@gmail.com>
2022-06-26 22:38:22 -0300

    modificada la cantidad maxima de instancias que se pueden ejecutar en simultaneo

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,12 @@ const functions = require("firebase-functions");
 const express = require('express');
 const app = express();
 const port = 5000;
-exports.app = functions.https.onRequest(app);
+
+exports.app = functions.runWith({
+    timeoutSeconds: 15,
+    maxInstances: 20, //0 es ilimitado
+}).https.onRequest(app);
+
 mutantesRouter = require('./routes/mutantes-route');
 
 app.use('/mutantes', mutantesRouter);
@@ -11,13 +16,3 @@ app.get('/', (req, res) => {
     res.send('Funcionando');
 });
 
-// app.listen(port, () => {
-//     console.log(`Mutantes listening `);
-// });
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
